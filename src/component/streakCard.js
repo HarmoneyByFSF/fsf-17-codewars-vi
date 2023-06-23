b2110e358cabf534e738f139457c44af8c7843b3
MRabill <71071872+MRabill@users.noreply.github.com>
2023-06-23 16:34:22 +0400

    streak bug fix

--- src/component/streakCard.js
+++ src/component/streakCard.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "../style/streakCard.css";
-import streak from "../img/streak.png";
+import coin from "../img/dollar_coin.png";
 import tick from "../img/tick.png";
 import streak_person from "../img/streak_person.png";
+import streakBack from "../img/streakBack.png";
 
 const StreakCard = () => {
   return (
     <div class="col-lg-6 text-center">
       <div class="d-flex justify-content-center">
         <div id="cardContainer" class="position-relative animated zoomIn">
-          <img class="img-fluid" src={streak} alt="" />
+          <img class="img-fluid" src={streakBack} alt="" />
           <div class="top-left-coin">
             <div class="streak-text">
-              <img src="img/dollar_coin.png" class="dollar-coin" alt="" />
+              <img src={coin} class="dollar-coin" alt="" />
               <div class="text">
                 <p>3 day streak</p>
                 <p>Way to go!</p>
