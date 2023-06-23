167096bf2e042c04d7197277c89b76b525c6fb3c
KavishMk11 <kavish17mojhoa@gmail.com>
2023-06-24 00:50:03 +0400

    Kavish commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,137 +1,154 @@
+import React, { useState } from "react";
 import "../style/App.css";
 import "../style/general.css";
 import "../style/bootstrap.min.css";
-import prt from "../img/portfolio-1.jpg";
-import coin from "../img/coin.webp";
-import logo from "../img/unnamed-removebg-preview.png";
 import Dashboard from "./Dashboard";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
 function App() {
-  const containerStyle = {
-    width: "1440px",
-    height: "130px",
-    position: "relative",
-    background: "#F6F6F6",
+  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
   };
 
-  const greetingStyle = {
-    width: "254.48px",
-    height: "84.36px",
-    left: "102.07px",
-    top: "38.89px",
-    position: "absolute",
-    color: "#898989",
-    fontSize: "46px",
-    fontFamily: "Poppins",
-    fontWeight: "500",
-    letterSpacing: "3.68px",
-    wordWrap: "break-word",
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
   };
 
-  const xpContainerStyle = {
-    left: "1014.82px",
-    top: "40.57px",
-    position: "absolute",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    gap: "41px",
-    display: "inline-flex",
-  };
-
-  const xpBadgeStyle = {
-    width: "109.12px",
-    height: "44.73px",
-    position: "relative",
-  };
-
-  const xpBackgroundStyle = {
-    width: "109.12px",
-    height: "44.73px",
-    left: "0px",
-    top: "0px",
-    position: "absolute",
-    background: "#8D64FE",
-    borderRadius: "54px",
-  };
-
-  const xpTextStyle = {
-    left: "14.52px",
-    top: "6.28px",
-    position: "absolute",
-    color: "white",
-    fontSize: "22px",
-    fontFamily: "Poppins",
-    fontWeight: "600",
-    letterSpacing: "1.76px",
-    wordWrap: "break-word",
-  };
-
-  const levelContainerStyle = {
-    width: "120.90px",
-    height: "53.49px",
-    position: "relative",
-  };
-
-  const levelBackgroundStyle = {
-    width: "106.36px",
-    height: "44.50px",
-    left: "14.55px",
-    top: "4.45px",
-    position: "absolute",
-    background: "#F985AF",
-    boxShadow: "0px 4px 11px #F985AF",
-    borderRadius: "50px",
-  };
-
-  const levelTextStyle = {
-    width: "33.78px",
-    height: "32.18px",
-    left: "70.60px",
-    top: "10.73px",
-    position: "absolute",
-    color: "white",
-    fontSize: "22px",
-    fontFamily: "Poppins",
-    fontWeight: "600",
-    letterSpacing: "1.76px",
-    wordWrap: "break-word",
-  };
-
-  const dollarImageStyle = {
-    width: "53.49px",
-    height: "53.49px",
-    left: "0px",
-    top: "0px",
-    position: "absolute",
-  };
-
-  const profileImageStyle = {
-    width: "68.36px",
-    height: "68.36px",
-    borderRadius: "50px",
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
 
   return (
     <div>
-      <div style={containerStyle}>
-        <div style={greetingStyle}>Hi, Rabill</div>
-        <div style={xpContainerStyle}>
-          <div style={xpBadgeStyle}>
-            <div style={xpBackgroundStyle}></div>
-            <div style={xpTextStyle}>340 XP</div>
-          </div>
-          <div style={levelContainerStyle}>
-            <div style={levelBackgroundStyle}></div>
-            <div style={levelTextStyle}>34</div>
-            <img style={dollarImageStyle} src="img/dollar 1.png" alt="dollar" />
-          </div>
-          <img
-            style={profileImageStyle}
-            src="img/testimonial-1.jpg"
-            alt="profile"
-          />
-        </div>
-      </div>
+      <br></br>
+      <br></br>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              DigiExpense
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+              </Menu>
+            </Box>
+            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
       <div
         style={{
           backgroundColor: "#F6F6F6",
