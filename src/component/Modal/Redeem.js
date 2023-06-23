1b60fcc29a6a6e3e68177a0056328f96b9882bee
Ashfaaq29 <Ashfaaqmuaaz29@gmail.com>
2023-06-24 00:48:43 +0400

    kiching

--- src/component/Modal/Redeem.js
+++ src/component/Modal/Redeem.js
@@ -1,9 +1,15 @@
 import React, { useState, useRef, useEffect } from "react";
 import "../../style/InputModal.css";
 import successSound from "../../music/treasure.mp3";
+import cashingSound from "../../music/Cashing.mp3";
+import "../../style/redeem.css";
 import editBtn from "../../img/edit.png";
+import coin from "../../img/redeem_coins.png";
+import Confetti from "react-confetti";
 
 const Redeem = () => {
+
+  
   const [openSuccess, setOpenSuccess] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -22,6 +28,10 @@ const Redeem = () => {
 
   const audio = new Audio(successSound);
   audio.loop = false;
+
+
+  const cashingAudio = new Audio(cashingSound);
+  cashingAudio.loop = false;
 
   function saveDataToLocalStorage() {
     const existingData = JSON.parse(localStorage.getItem("Expenses"));
@@ -44,79 +54,115 @@ const Redeem = () => {
 
     console.log(Array.isArray(existingData));
   }
-
+  const { width, height } = 2030;
   return (
     <div>
       {/* <button >Show Pop-up</button> */}
       {/* <img src={editBtn} alt="Edit" onClick={() => setOpenSuccess(true)} /> */}
-      <div class="button" onClick={() => setOpenSuccess(true)}>
+      <div class="button"  onClick={() => {
+          audio.loop = false;
+          audio.play();
+          setOpenSuccess(true);
+        }}>
         Redeem
       </div>
 
       <div
         id="popup-container"
         class="popup-container"
-        style={{ display: openSuccess ? "flex" : "none" }}
-      >
+        style={{ display: openSuccess ? "flex" : "none" }}>  
+        
+        <Confetti width={width} height={height} />
+
+
         <div class="popup-content">
           <div class="close-button" onClick={() => setOpenSuccess(false)}>
             &#x2716;
           </div>
           <h2 class="popup-title" style={{ color: "#484848" }}>
-            Add Expenses
+            Redeem Coins
           </h2>
+          <div class="redeem-content">
+          <div class="stock-container">
+            <div class="content">
+            <div class="stock-details">
 
-          <label class="popup-subtitle">Title</label>
-          <br />
-          <input
-            class="expense-textfield"
-            type=""
-            id="text1"
-            value={title}
-            placeholder="Enter bill title"
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <br />
+              <div class="coins-amount-container">
 
-          <label class="popup-subtitle">Description</label>
-          <br />
-          <input
-            class="expense-textfield"
-            type="text"
-            id="text2"
-            value={description}
-            placeholder="Enter bill description"
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          <br />
+              <img src={coin} class="dollar-coin" alt=""  style={{width:"35px", height:"35px"}}/>
+               <div class="coins-amount"><p> 34 coins</p></div>
 
-          <label class="popup-subtitle">Amount</label>
-          <br />
-          <input
-            class="expense-textfield"
-            type="text"
-            id="text3"
-            value={amount}
-            placeholder="Enter bill amount"
-            onChange={(e) => setAmount(e.target.value)}
-          />
-          <br />
+               </div>
 
-          <button
-            class="buttonContinue"
-            onClick={() => {
-              saveDataToLocalStorage();
-              retrieveAllDataFromLocalStorage();
-              audio.loop = false;
-              audio.play();
-            }}
-          >
-            Add Expense
-          </button>
+               <div class="stock-name"><p>Allsport shoes</p></div>
+               
+            </div>
 
-          {/* <script type="text/javascript">
-                    
-                </script> */}
+          
+            <div class="buy-button">
+                <p onClick={() => {
+          cashingAudio.loop = false;
+          cashingAudio.play();
+          setOpenSuccess(true);
+        }}>Use</p> 
+            </div>
+            </div>
+          </div>
+          <div class="stock-container">
+            <div class="content">
+            <div class="stock-details">
+
+              <div class="coins-amount-container">
+
+              <img src={coin} class="dollar-coin" alt=""  style={{width:"35px", height:"35px"}}/>
+               <div class="coins-amount"><p> 15 coins</p></div>
+
+               </div>
+
+               <div class="stock-name"><p>Magasin 58 rrr</p></div>
+               
+            </div>
+
+          
+            <div class="buy-button">
+                <p onClick={() => {
+          cashingAudio.loop = false;
+          cashingAudio.play();
+          setOpenSuccess(true);
+        }}>Use</p> 
+            </div>
+            </div>
+          </div>
+          <div class="stock-container">
+            <div class="content">
+            <div class="stock-details">
+
+              <div class="coins-amount-container">
+
+              <img src={coin} class="dollar-coin" alt=""  style={{width:"35px", height:"35px"}}/>
+               <div class="coins-amount"><p> 45 coins</p></div>
+
+               </div>
+
+               <div class="stock-name"><p>CitySport shoes</p></div>
+               
+            </div>
+
+          
+            <div class="buy-button">
+                <p onClick={() => {
+          cashingAudio.loop = false;
+          cashingAudio.play();
+          setOpenSuccess(true);
+        }}>Use</p> 
+            </div>
+            </div>
+            </div>
+          </div>
+          
+
+
+          
         </div>
       </div>
     </div>
